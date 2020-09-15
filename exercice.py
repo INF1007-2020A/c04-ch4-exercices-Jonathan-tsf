b1ca870f4bde33884eaa016f03aa5a5b2cb48257
Jonathan-tsf <jonat.tapiero@gmail.com>
2020-09-15 22:47:37 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,8 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     for i in len(string):
-        if 
+        if string[i] == old_char:
+            
 
 
 def get_number_of_char(string: str, char: str) -> int:
