1e284ae6252090a31ccf1693b2311f9bbb56a954
Jonathan-tsf <jonat.tapiero@gmail.com>
2020-09-15 22:51:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,6 +31,7 @@ def get_number_of_char(string: str, char: str) -> int:
     for i in range(len(string)):
         if string[i] == char:
             i += 1
+            print()
     return i 
 
 def get_number_of_words(sentence: str, word: str) -> int:
